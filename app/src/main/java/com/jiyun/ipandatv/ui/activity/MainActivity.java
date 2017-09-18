07caa0155fedda758d5eb2c6a2daaee8ea6fa0d9
wayufi <929272840@qq.com>
2017-09-18 10:20:04 +0800

    第一个界面和第二个界面上传1

--- app/src/main/java/com/jiyun/ipandatv/ui/activity/MainActivity.java
+++ app/src/main/java/com/jiyun/ipandatv/ui/activity/MainActivity.java
@@ -4,11 +4,6 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.view.KeyEvent;
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -31,7 +26,6 @@ import butterknife.OnClick;
 import static com.jiyun.ipandatv.R.id.rbut_China;
 import static com.jiyun.ipandatv.R.id.rbut_Live;
 import static com.jiyun.ipandatv.R.id.rbut_home;
-import static com.jiyun.ipandatv.R.id.rg_froup;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
@@ -61,17 +55,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     RadioGroup rgFroup;
     @Bind(R.id.tv_title)
     TextView tvTitle;
-    // 定义一个变量，来标识是否退出
-    private static boolean isExit = false;
 
-    private Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            isExit = false;
-        }
-    };
     // 定义一个变量，来标识是否退出
     private static boolean isExit = false;
 
@@ -221,27 +205,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
-    //对返回键进行监听
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            exit();
-            return false;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
 
-    private void exit() {
-        if (!isExit) {
-            isExit = true;
-            Toast.makeText(getApplicationContext(), "再按一次退出程序",
-                    Toast.LENGTH_SHORT).show();
-            // 利用handler延迟发送更改状态信息
-            mHandler.sendEmptyMessageDelayed(0, 2000);
-        } else {
-            finish();
-            System.exit(0);
-        }
-    }
 
 }
